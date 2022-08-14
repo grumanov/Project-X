4c10d3bc23d6feb295c3027c989e04f3f0d389e6
grumanov <48724824+grumanov@users.noreply.github.com>
2022-08-14 18:02:30 +0200

    minimal basic stuff

--- scripts.js
+++ scripts.js
@@ -1,4 +1,16 @@
-function Wechsel() {
-	document.getElementById("Text").innerHTML = "Hallo";
-	console.log("ping");
+let counter = 0;
+
+function klick() {
+
+	counter = counter + 1;
+	document.getElementById("showcounter").innerHTML = "Counter: " + counter;
 }
+
+
+ function startup() {
+ 	//TODO
+ 	//für spätere Dinge die direkt beim Start ausgeführt werden sollen
+ 	//Spielstand laden etc
+ }
+
+
